418b91c7722d29944b9f760db8b31dc1d0ee1d4f
qorrnlgus95 <qorrnlgus95@maver.com>
2022-07-07 13:45:29 +0900

    test

--- src/main/java/com/example/demo/BoardForGoeApplication.java
+++ src/main/java/com/example/demo/BoardForGoeApplication.java
@@ -8,6 +8,7 @@ public class BoardForGoeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardForGoeApplication.class, args);
+        System.out.println("hello board goe");
     }
 
 }
